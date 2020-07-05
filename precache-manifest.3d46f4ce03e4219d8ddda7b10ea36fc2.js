175e20984d4ce1e36cfe6a105b701a19e9e68071
Cira Simion Florin <simiflorin333@yahoo.com>
2020-07-05 19:38:19 +0300

    Updates

---
+++ precache-manifest.3d46f4ce03e4219d8ddda7b10ea36fc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004a17142d084862b4a7c323cd6f545c",
+    "url": "/Pneumonia-detection-web-app/index.html"
+  },
+  {
+    "revision": "15f6cbf51fbdd72c3112",
+    "url": "/Pneumonia-detection-web-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "73f8236f9a3235c8244b",
+    "url": "/Pneumonia-detection-web-app/static/css/main.6f5dedcc.chunk.css"
+  },
+  {
+    "revision": "15f6cbf51fbdd72c3112",
+    "url": "/Pneumonia-detection-web-app/static/js/2.c55f0b73.chunk.js"
+  },
+  {
+    "revision": "2c772112d49e44d86d4cc822b1410426",
+    "url": "/Pneumonia-detection-web-app/static/js/2.c55f0b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f8236f9a3235c8244b",
+    "url": "/Pneumonia-detection-web-app/static/js/main.dfa1fa69.chunk.js"
+  },
+  {
+    "revision": "e86652d909480ab5c117",
+    "url": "/Pneumonia-detection-web-app/static/js/runtime-main.7dca295e.js"
+  },
+  {
+    "revision": "c3d30ca62f75b9476fcaaae336634469",
+    "url": "/Pneumonia-detection-web-app/static/media/pneumonia-background-image.c3d30ca6.jpg"
+  }
+]);
